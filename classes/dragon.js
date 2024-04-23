b89f95b067616cb404b3c5c94f36d807518cefcb
Laiba Afzal <laiba.a1298@gmail.com>
2024-04-23 17:35:05 -0400

    dragons

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,23 @@
 // Your code here 
 
+class Dragon{
+  constructor(name, color){
+    this.name = name;
+    this.color = color;
+  }
+  breathesFire(){
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+  static getDragons(...dragons){
+    return dragons.map(dragon => dragon.name);
+  }
+}
+
+const puff = new Dragon("Puff", "green");
+console.log(puff);
+console.log(puff.breathesFire());
+
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
