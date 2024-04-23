17c438d7ee7173b2e3805fd96a2e42d7f07488b7
Steven <stevenglab@gmail.com>
2024-04-23 14:56:59 -0700

    evil-dragon complete

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,23 @@
 // Your code here 
+const Dragon = require('./dragon');
+
+class EvilDragon extends Dragon {
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    dontInviteThemOverForDinner() {
+        for (const activity of this.evilDoings) {
+            console.log(`${this.name} will ${activity}`);
+        }
+    }
+    burnsNemesis(){
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+    }
+
+
+}
+
+module.exports = EvilDragon;
